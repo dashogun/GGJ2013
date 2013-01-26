786860e81d2f8564abbeb651127dbff7eb1760c2
Daniel Shugan <shugands@gmail.com>
2013-01-26 15:42:23 -0500

    Added framework for classes.

---
+++ BloodyPipeDream/BloodyPipeDream/BloodyPipeDream/BloodPressure.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BloodyPipeDream
+{
+	class BloodPressure
+	{
+	}
+}
